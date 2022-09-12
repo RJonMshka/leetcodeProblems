57b79a197a5577c63dc22b559ac0cb631c17d5fb
Kumar <rkumar40@uic.edu>
2022-09-11 19:41:05 -0500

    added java single number easy problem

---
+++ Bit Manipulation/single-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = nums[0];
+        
+        if(nums.length > 1) {
+            for (int i = 1; i<nums.length; i++) {
+                result ^= nums[i];
+            }
+        }
+        
+        return result;
+        
+    }
+}
