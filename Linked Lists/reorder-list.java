0074e5c77aa5a8b5dc0625b961446aaa8af7bc57
Rajat Kumar <rajatkumar@Rajats-MacBook-Pro.local>
2023-01-12 18:07:47 -0600

    LC 143 reorder linked list

---
+++ Linked Lists/reorder-list.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // slow will be at middle
+        ListNode firstList = head;
+        ListNode secondList = slow.next;
+        slow.next = null;
+        ListNode prevSecond = null;
+
+        // now reverse the second list
+        while(secondList != null) {
+            ListNode temp = secondList.next;
+            secondList.next = prevSecond;
+            prevSecond = secondList;
+            secondList = temp;
+        }
+        
+        ListNode start = head;
+        
+        while(prevSecond != null) {
+            ListNode next = start.next;
+            start.next = prevSecond;
+            prevSecond = prevSecond.next;
+            start.next.next = next;
+            start = start.next.next;
+        }
+        
+    }
+}
