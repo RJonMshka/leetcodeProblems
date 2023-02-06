cabf44a308c4f8c74c6fb1ce7b3c9ccfb5276726
Rajat Kumar <rajat1996kumar@gmail.com>
2023-02-06 07:30:16 -0600

    shuffle array added

---
+++ Arrays/shuffle-array.java
@@ -0,0 +1,14 @@
+// LC 1470: Shuffle the Array
+
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] output = new int[2 * n];
+
+        for(int i = 0; i < n; i++) {
+            output[2 * i] = nums[i];
+            output[2 * i + 1] = nums[n + i];
+        }
+
+        return output;
+    }
+}
